a5e29a779e168644fc542fb2a54f7cd75056b38d
Joseph Birkner <j.birkner@klebert-engineering.de>
2021-03-05 17:06:45 +0100

    Foundation for pyzswagcl wheel.

---
+++ libs/pyzswagcl/pyzswagcl.cpp
@@ -0,0 +1,8 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+
+namespace py = pybind11;
+using namespace py::literals;
+
